0a55fc69504bc8a9b8957b429aa572cf7dc429e3
AmyFong82 <amyhfong@gmail.com>
2020-12-23 16:38:39 -0500

    used validation with users/new form

--- party-planning-app/app/models/user.rb
+++ party-planning-app/app/models/user.rb
@@ -12,6 +12,6 @@ class User < ApplicationRecord
 	validates :first_name, presence: true
 	validates :last_name, presence: true
 	validates :email, uniqueness: true
-	validates :phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "Please match the required format." }
+	validates :phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "must match the required format." }
 
 end
