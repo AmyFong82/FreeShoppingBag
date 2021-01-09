a2910d72a24a2c4d882e498f57d93c1fc52e3a78
AmyFong82 <amyhfong@gmail.com>
2021-01-09 00:10:20 -0500

    working ticket update form.

--- party-planning-app/app/controllers/tickets_controller.rb
+++ party-planning-app/app/controllers/tickets_controller.rb
@@ -1,12 +1,20 @@
 class TicketsController < ApplicationController
 
 	def create
-		@ticket = Ticket.create(party_id: params[:party_id], user_id: params[:user_id], num_of_attendees: params[:ticket][:num_of_attendees])
-		redirect_to user_party_path(current_user, @ticket.party)
+		if logged_in?
+			@ticket = Ticket.create(party_id: params[:party_id], user_id: params[:user_id], num_of_attendees: params[:ticket][:num_of_attendees])
+			redirect_to user_party_path(current_user, @ticket.party)
+		end
 	end
 
+	def update
+		if logged_in?
+			@ticket = Ticket.find_by(user_id: params[:user_id], party_id: params[:party_id])
+			@ticket.update(num_of_attendees: params[:ticket][:num_of_attendees])
+			redirect_to user_party_path(current_user, @ticket.party)
+		end
+	end
 
-	private
 
 
 end
